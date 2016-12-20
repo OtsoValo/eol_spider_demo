229c6abec3a3218f11f4c8f7bec06255cda08ebd
zhangzhiyan <693946948@qq.com>
2016-12-20 23:27:15 +0800

    init

---
+++ eof_spider/func.py
@@ -0,0 +1,5 @@
+import datetime
+
+
+def mysql_datetime():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S');
---
+++ eof_spider/items.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class CandidateBasicItem(Item):
+    # define the fields for your item here like:
+    # name = Field()
+    country_id = Field()
+    college_id = Field()
+    discipline_id = Field()
+    fullname = Field()
+    academic_title = Field()
+    other_title = Field()
+    nationality = Field()
+    email = Field()
+    phonenumber = Field()
+    external_link = Field()
+    experience = Field()
+    desc = Field()
+    extra = Field()
+    avatar_url = Field()
+    discipline_desc = Field()
+    create_time = Field()
+
+
+class CandidateEducationItem(Item):
+    cb_id = Field()
+    college = Field()
+    discipline = Field()
+    start_time = Field()
+    end_time = Field()
+    duration = Field()
+    degree = Field()
+    desc = Field()
+    create_time = Field()
+
+
+class CandidateResearchItem(Item):
+    cb_id = Field()
+    interests = Field()
+    current_research = Field()
+    research_summary = Field()
+    create_time = Field()
+
+
+class CandidatePublicationsItem(Item):
+    cb_id = Field()
+    publications = Field()
+    create_time = Field()
+
+
+class CandidateCoursesItem(Item):
+    cc_id = Field()
+    cb_id = Field()
+    courses_no = Field()
+    courses_desc = Field()
+    create_time = Field()
+
+
+class CandidateWorkexperienceItem(Item):
+    cw_id = Field()
+    cb_id = Field()
+    job_title = Field()
+    company = Field()
+    start_time = Field()
+    end_time = Field()
+    duration = Field()
+    desc = Field()
+    create_time = Field()
+
